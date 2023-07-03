23990c5207bde9efc614a47bd5a0f84bb49c8efa
Je <joimh@naver.com>
2023-07-03 15:27:37 +0900

    삭제 기능 추가

--- app/list/list-item.tsx
+++ app/list/list-item.tsx
@@ -10,11 +10,47 @@ export default function ListItem({ data }: {
 
 	const [list, dispatchList] = useState(data);
 
+	const [deletedItemId, setDeletedItemId] = useState(null as string);
+
+	const handleDelete = (itemId: string) => {
+		console.log(itemId);
+		fetch(`/api/post/delete`, {
+			method: 'POST',
+			body: itemId
+		}).then(res => {
+			if (res.status === 200) {
+				return res;
+			} else {
+				throw new Error('오류');
+			}
+		}).then((res) => {
+			const filtered = list.filter((item: Post) => item.id !== itemId);
+			setDeletedItemId(itemId);
+			setTimeout(() => {
+				dispatchList(filtered);
+			}, 600);
+
+		}).catch((err) => { //인터넷문제 등으로 실패시 실행할코드
+			console.log(err);
+		});
+	};
+
+	useEffect(() => {
+		if (deletedItemId) {
+			const timeout = setTimeout(() => {
+				setDeletedItemId(null);
+			}, 500); // 애니메이션 지속시간인 0.5초 후에 삭제된 아이템 ID 초기화
+
+			return () => clearTimeout(timeout);
+		}
+	}, [deletedItemId]);
+
 	return (
 		<div>
-
+			<div>Id : {deletedItemId}</div>
 			{list.map((item: unknown, index: number) => (
-				<div className="list-item p-5 border-2 border-gray-500 mt-1" key={index}>
+				<div className={`list-item p-5 border-2 border-gray-500 mt-1 ${item.id === deletedItemId ? 'hide' : ''}`}
+					 key={index}>
 					<Link href={`/detail/${(item as Post).id}`} >
 						<h4 className="text-xl font-semibold">{(item as Post).title}</h4>
 						<p>{(item as Post).content}</p>
@@ -22,17 +58,9 @@ export default function ListItem({ data }: {
 					<Link className="bg-green-800 mt-2 p-2 text-white inline-block"
 						  href={`/edit/${(item as Post).id}`}>수정</Link>
 					<span className="bg-red-800 mt-2 ms-2 p-2 text-white inline-block cursor-pointer"
-						  onClick={() => {
+						  onClick={(e) => {
 							  const deleteId = (item as Post).id;
-							  fetch(`/api/post/delete`, {
-								  method: 'POST',
-								  body: deleteId
-							  }).then((res) => {
-								  const filtered = list.filter((item: Post) => {
-									  return deleteId !== (item as Post).id;
-								  });
-								  dispatchList(filtered);
-							  });
+							  handleDelete(deleteId)
 						  }}
 					>삭제</span>
 				</div>
--- pages/api/post/delete.tsx
+++ pages/api/post/delete.tsx
@@ -2,14 +2,26 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import { connectDB } from "@/util/database";
 import { ObjectId } from "bson";
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-	const id = req.body;
+	let id = req.body;
+	const objectId = new ObjectId(id);
 
 	if (req.method === "POST") {
-		// const client = await connectDB;
-		// const db = client.db("db-stelive");
-		// let result = await db.collection('post')
-		// 	.deleteOne({ _id: new ObjectId(id) })
-		return res.status(200).json('처리완료');
+		try {
+			const client = await connectDB;
+			const db = client.db("db-stelive");
+
+			let result = await db.collection('post')
+				.deleteOne({ _id: objectId })
+				.then((r) => {
+					// throw new Error("게시물을 찾을 수 없습니다.");
+					return res.status(200).json('처리완료');
+				})
+				.catch((err) => {
+					return res.status(500).json('MongoDB 처리 오류');
+				});
+		} catch (error) {
+			return res.status(500).json('처리실패');
+		}
 	}
 
 
