c8019ab016f655b83c58eededaf083731c2b91d9
Je <joimh@naver.com>
2023-07-11 11:58:44 +0900

    -loading.tsx 추가

---
+++ app/detail/[id]/loading.tsx
@@ -0,0 +1,18 @@
+
+export default function Loading() {
+	return(
+		<div>
+			로딩중...
+		</div>
+	)
+}
+
+// export default function Loading() {
+// 	render() {
+// 		return (
+// 			<div>
+// 				<h1>Loading...</h1>
+// 			</div>
+// 		)
+// 	}
+// }
